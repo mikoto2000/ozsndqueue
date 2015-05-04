1276466ed91e6c9af614e883a086cb06bc267224
mikoto2000 <mikoto2000@gmail.com>
2015-05-04 09:20:54 +0900

    Added Play method in SoundManager.

--- SoundManager.go
+++ SoundManager.go
@@ -1,5 +1,9 @@
 package ozsndqueue
 
+import (
+	"sort"
+)
+
 type SoundManager struct {
 	priorityQueue map[int][]string
 }
@@ -20,4 +24,64 @@ func (this *SoundManager) Put(fileUri string, queueNumber int) {
 	}
 
 	this.priorityQueue[queueNumber] = append(this.priorityQueue[queueNumber], fileUri)
+}
+
+// Play, 一番優先順位の高いファイルを再生する。
+func (this *SoundManager) Play() {
+	if len(this.priorityQueue) == 0 {
+		return
+	}
+
+	this.prioritiedDequeue()
+}
+
+// prioritiedDequeue, キュー郡から一番優先順位の高いファイル名を取得する。
+func (this *SoundManager) prioritiedDequeue() string {
+	// そもそも map に要素がないのであれば空文字を返却する。
+	if len(this.priorityQueue) == 0 {
+		return ""
+	}
+
+	// キーを抜き出してスライスに詰める
+	keys := []int{}
+	for key := range this.priorityQueue {
+		 keys = append(keys, key)
+	}
+
+	// 優先度順にソート
+	sort.Ints(keys)
+
+	// 優先度順に探索して、最初に見つかったものを返却する。
+	// 返却した要素は削除する。
+	for _, value := range keys {
+		fileUri, fileUris := dequeue(this.priorityQueue[value])
+
+		// キューの更新
+		if (len(fileUris) == 0) {
+			// キューが空になったら map から削除する
+			delete(this.priorityQueue, value)
+		} else {
+			// 空でなければ Dequeue 済みのスライスと入れ替える
+			this.priorityQueue[value] = fileUris
+		}
+
+		// Dequeue した fuleUri を返却。
+		return fileUri
+	}
+
+	// 見つからないことは無いはずだけど、
+	// 見つからなければから文字を返す。
+	return ""
+}
+
+// ファイル名リスト(スライス)から要素を Dequeue する。
+func dequeue(fileUris []string) (string, []string) {
+	// そもそも要素がないのであれば空文字を返却する。
+	if len(fileUris) == 0 {
+		return "", fileUris
+	}
+
+	// 先頭 1 要素とそれ以降のスライスを返却。
+	// この前に長さチェックしてるからそのまま返せるはず。
+	return fileUris[0], fileUris[1:len(fileUris)]
 }
--- _example/SoundManager.go
+++ _example/SoundManager.go
@@ -15,4 +15,28 @@ func main() {
 	soundManager.Put("PATH_TO_FILE4", -1)
 
 	fmt.Println(soundManager)
+
+
+	soundManager = ozsndqueue.CreateSoundManager()
+	soundManager.Put("PATH_TO_FILE1", 0)
+	fmt.Println(soundManager)
+	soundManager.Play()
+	fmt.Println(soundManager)
+
+	soundManager.Put("PATH_TO_FILE1", 0)
+	soundManager.Put("PATH_TO_FILE2", 0)
+	soundManager.Put("PATH_TO_FILE3", 1)
+	soundManager.Put("PATH_TO_FILE4", 2)
+	soundManager.Put("PATH_TO_FILE5", 2)
+	fmt.Println(soundManager)
+	soundManager.Play()
+	fmt.Println(soundManager)
+	soundManager.Play()
+	fmt.Println(soundManager)
+	soundManager.Play()
+	fmt.Println(soundManager)
+	soundManager.Play()
+	fmt.Println(soundManager)
+	soundManager.Play()
+	fmt.Println(soundManager)
 }
